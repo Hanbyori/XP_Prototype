57ca921bd7be585f4b13a9e3af5b822fc06d5a83
Hanbyori <sinsarigger@gmail.com>
2022-09-04 13:35:04 +0900

    commit

--- Assets/Scripts/Flame.cs
+++ Assets/Scripts/Flame.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Flame : MonoBehaviour
@@ -10,8 +11,29 @@ public class Flame : MonoBehaviour
 	{
 		if (other.tag == "Monster")
 		{
-			if (!isAttack) other.GetComponent<Monster>().monstarHP -= 1f;
+			if (!isAttack)
+			{
+				if (transform.parent.GetComponent<Player>().type == "red" && other.name == "Green")
+					other.GetComponent<Monster>().monstarHP -= 1.5f;
+				else if (transform.parent.GetComponent<Player>().type == "green" && other.name == "Blue")
+					other.GetComponent<Monster>().monstarHP -= 1.5f;
+				else if (transform.parent.GetComponent<Player>().type == "blue" && other.name == "Red")
+					other.GetComponent<Monster>().monstarHP -= 1.5f;
+				else
+					other.GetComponent<Monster>().monstarHP -= 1.0f;
+			}
 			StartCoroutine(attack());
+		}
+
+		else if (other.tag == "Row")
+		{
+			if (transform.parent.GetComponent<Player>().type == "red" && other.name == "GreenT")
+				other.transform.GetChild(0).GetComponent<TextMeshPro>().text = "X1.5";
+			else if (transform.parent.GetComponent<Player>().type == "green" && other.name == "BlueT")
+				other.transform.GetChild(0).GetComponent<TextMeshPro>().text = "X1.5";
+			else if (transform.parent.GetComponent<Player>().type == "blue" && other.name == "RedT")
+				other.transform.GetChild(0).GetComponent<TextMeshPro>().text = "X1.5";
+			else other.transform.GetChild(0).GetComponent<TextMeshPro>().text = "X1.0";
 		}
 	}
 
